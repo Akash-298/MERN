62e8e3c75a4870bbed272941709e32a30bd60007
Akash-298 <akash.c.c298@gmail.com>
2023-07-28 22:23:19 +0530

    react todo app

---
+++ ToDoReact App/toDo/src/todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const todo = ()=>{
+    return  (
+        <div className='main-div'>
+            <div className='child-div'> 
+                    <figure> 
+                        <img src='./'  alt='no image found'>  </img>
+                        <figcaption>Add Your List Here</figcaption>
+
+                    </figure>
+
+                    <div className='addItems'> </div>
+            </div>
+            
+        </div>
+
+    )
+    }       
