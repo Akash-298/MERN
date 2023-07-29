b06ee056261d24c894dd80302ec4da66e76cddf4
Akash-298 <akash.c.c298@gmail.com>
2023-07-29 21:23:07 +0530

    fixed the delete functionality part

--- react app new 2/todo/src/components/todo.js
+++ react app new 2/todo/src/components/todo.js
@@ -1,43 +1,59 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
-const Todo = () => {
-      const[inputdata,setInputData] =useState("");
-      const[items,setItems] = useState(getLocalData());
 
-      // adding local storage
-const getLocalData=()=>{
-  const lists = localStorage.getItem("todolist")
-  if(lists){ return JSON.parse(lists)}
-   else{
+
+const getLocalData = () => {
+  const lists = localStorage.getItem("todolist");
+  if (lists) {
+    return JSON.parse(lists);
+  } else {
     return [];
-   }
+  }
+};
+const Todo = () => {
+  const [inputdata, setInputData] = useState("");
+  const [items, setItems] = useState(getLocalData());
+
+  // adding local storage
+
+  useEffect(() => {
+    localStorage.setItem("todolist", JSON.stringify(items));
+  }, [items]);
+  const addItem = () => {
+    if (!inputdata) {
+      alert("please fill data");
+    } else {
+      const myNewInputData = {
+        id: new Date().getTime().toString(),
+        name: inputdata,
+      };
+      setItems([...items, myNewInputData]);
+      setInputData("");
+    }
+  };
+// edit the items
+
+const editItem=(index)=>{
+        const item_todo_edited = items.find((currElem)=>{
+          return 
+        })
 }
-      useEffect(()=>{
-        localStorage.setItem("todolist",JSON.stringify(items))
-      },[items])
-      const addItem = () =>{
-        if(!inputdata){
-          alert("please fill data")
-        }else{
-          const myNewInputData={
-            id:new Date().getTime().toString(),
-            name:inputdata
-          }
-          setItems([...items,myNewInputData])
-          setInputData("")
-        }
-      }
-                  // delete item section
-        const deleteItem=(index)=>{
-          const updatedItens = items.filter((currElem)=>{
-            return currElem != index;
-          })
-          setItems(updatedItens);
-        //  const           
-        }
-        const removeAll=()=>{
-          setItems([]);
-        }
+
+  // delete item section
+  const deleteItem = (index) => {
+    const updatedItens = items.filter((currElem) => {
+      return currElem.id != index;
+    });
+    setItems(updatedItens);
+    //  const
+  };
+
+
+
+  // remove all elements
+  const removeAll = () => {
+    setItems([]);
+  };
   return (
     <div className="main-div">
       <div className="child-div">
@@ -51,33 +67,34 @@ const getLocalData=()=>{
             type="text"
             placeholder="👉 Add Items"
             className="form-control"
-            value={inputdata }
-            onChange={(event)=>setInputData(event.target.value)}
+            value={inputdata}
+            onChange={(event) => setInputData(event.target.value)}
           ></input>
           <i class="fa fa-plus" aria-hidden="true" onClick={addItem}></i>
         </div>
         <div className="showItems">
-          {
-            items.map((currElem) => {
-                return(
-                  <div className="eachItem" key={currElem.id}>
-                  <h3>{currElem.name}</h3>
-                  <div className="todo-btn">
-                    <i class="far fa-edit add-btn"> </i>
-                    {/* <i class="fa fa-minus" aria-hidden="true"></i> */}
-                    {/* <i class="fa fa-trash" aria-hidden="true"></i> */}
-                    <i class="fa fa-ban" aria-hidden="true" onClick={()=>
-                            deleteItem(currElem.id)
-                    }></i>
-                  </div>
+          {items.map((currElem) => {
+            return (
+              <div className="eachItem" key={currElem.id}>
+                <h3>{currElem.name}</h3>
+                <div className="todo-btn">
+                  <i class="far fa-edit add-btn" onClick={()=>editItem(currElem.id)}> </i>
+                  {/* <i class="fa fa-minus" aria-hidden="true"></i> */}
+                  {/* <i class="fa fa-trash" aria-hidden="true"></i> */}
+                  <i
+                    class="fa fa-ban"
+                    aria-hidden="true"
+                    onClick={() => deleteItem(currElem.id)}
+                  ></i>
                 </div>
-                )
-            })
-          }
-         
+              </div>
+            );
+          })}
         </div>
         <div className="showItems">
-          <button className="btn effect04" onClick={removeAll}>Remove all</button>
+          <button className="btn effect04" onClick={removeAll}>
+            Remove all
+          </button>
         </div>
       </div>
     </div>
