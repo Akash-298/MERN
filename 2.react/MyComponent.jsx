f3813726a190ffdf36f63b763558d5486e217cb4
AKASH CHIKHALONDE <105929312+Akash-298@users.noreply.github.com>
2023-08-14 21:00:55 +0530

    Create MyComponent.jsx

---
+++ 2.react/MyComponent.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const MyComponent = () => {
+  const [data, setData] = useState([
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ]);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <span>{item.id}: {item.name}</span>
+          <button onClick={() => handleDelete(item.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MyComponent;
+
