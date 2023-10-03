9afdfe27d4f199dc0551aca3b654cd1418b22574
AKASH CHIKHALONDE <105929312+Akash-298@users.noreply.github.com>
2023-10-03 15:15:15 +0530

    Create sort.jsx

---
+++ 2.react/sort.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [alphabets, setAlphabets] = useState(['e', 'b', 'c', 'a', 'd']);
+
+  useEffect(() => {
+    const sortedAlphabets = [...alphabets].sort();
+    setAlphabets(sortedAlphabets);
+  }, []);
+
+  return (
+    <div>
+      <h2>Sorted Alphabets:</h2>
+      {alphabets.map((alphabet, index) => (
+        <p key={index}>{alphabet}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
